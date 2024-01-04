e1373da69ee4eeade2944b2e06b814c23c1b20e6
Stan Vlad <63295109+vstan02@users.noreply.github.com>
2024-01-04 21:35:02 +0200

    Updated the script

--- archy.sh
+++ archy.sh
@@ -1,37 +1,32 @@
-# sudo pacman -Syu --noconfirm
-# sudo pacman -S linux-lts-headers intel-ucode git clang curl wget zsh neovim xorg xorg-server xdg-user-dirs --noconfirm
-# xdg-user-dirs-update
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo pacman -Sy linux-lts-headers intel-ucode git clang curl wget zsh neovim xorg xorg-server xdg-user-dirs gnome-shell gdm gnome-terminal gnome-control-center gnome-tweaks gnome-shell-extensions gnome-system-monitor eog celluloid gnome-text-editor nautilus vivaldi vivaldi-ffmpeg-codecs libnotify breeze papirus-icon-theme gnome-backgrounds power-profiles-daemon gnome-calculator ttf-caladea ttf-carlito ttf-dejavu ttf-liberation noto-fonts ttf-opensans otf-overpass ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore pipewire pipewire-pulse pipewire-audio wireplumber bluez bluez-utils --noconfirm
 
-# mkdir ~/.config/nvim
-# cp ./assets/vimrc ~/.config/nvim/init.vim
-# cp ./assets/zshrc ~/.zshrc
+xdg-user-dirs-update
 
-# sudo pacman -S gnome-shell gdm gnome-terminal gnome-control-center gnome-tweaks gnome-shell-extensions gnome-system-monitor eog celluloid gnome-text-editor nautillus vivaldi vivaldi-ffmpeg-codecs libnotify breeze papipus-icon-theme gnome-backgrouds power-profiles-daemon gnome-calculator --noconfirm
+cd ~/Downloads
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -sri
+cd ..
+sudo rm -R yay
+cd ~
+yay -S gdm-settings
 
-# cd ~/Downloads
-# git clone https://github.com/vinceliuice/Graphite-gtk-theme.git
-# cd Graphite-gtk-theme
-# ./install.sh -t blue -c dark --tweaks nord darker rimless normal
-# ./install.sh -l -t blue -c dark --tweaks nord darker rimless normal
-# sudo ./install.sh -t blue -c dark --tweaks nord darker rimless normal
-# sudo ./install.sh -l -t blue -c dark --tweaks nord darker rimless normal
-# sudo ./install.sh -g -t blue -c dark --tweaks nord darker rimless normal
-# cd ..
-# sudo rm -R Graphite-gtk-theme
-# cd ~
+cd ~/Downloads
+git clone https://github.com/vinceliuice/Graphite-gtk-theme.git
+cd Graphite-gtk-theme
+./install.sh -t blue -c dark --tweaks nord darker rimless normal
+./install.sh -l -t blue -c dark --tweaks nord darker rimless normal
+sudo ./install.sh -t blue -c dark --tweaks nord darker rimless normal
+sudo ./install.sh -l -t blue -c dark --tweaks nord darker rimless normal
+sudo ./install.sh -g -t blue -c dark --tweaks nord darker rimless normal
+cd ..
+sudo rm -R Graphite-gtk-theme
+cd ~
 
-# cd ~/Downloads
-# git clone https://aur.archlinux.org/yay.git
-# cd yay
-# makepkg -sri
-# cd ..
-# sudo rm -R yay
-# cd ~
-# yay -S gdm-settings
+sudo systemctl enable gdm.service
+sudo systemctl enable bluetooth.service
 
-# sudo pacman -S ttf-caladea ttf-carlito ttf-dejavu ttf-liberation noto-fonts ttf-opensans otf-overpass ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family --noconfirm
-# sudo pacman -S a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore --noconfirm
-
-# sudo systemctl enable gdm.service
-
+mkdir ~/.config/nvim
+cp ./assets/vimrc ~/.config/nvim/init.vim
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp ./assets/zshrc ~/.zshrc
