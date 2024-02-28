6a9817c83c1442c40304e90988a705a8f2406711
Stan Vlad <63295109+vstan02@users.noreply.github.com>
2024-02-28 19:19:46 +0200

    Update archy.sh

--- archy.sh
+++ archy.sh
@@ -1,8 +1,21 @@
-sudo pacman -Sy linux-lts-headers intel-ucode git clang curl wget zsh neovim xorg xorg-server xdg-user-dirs gnome-shell gdm gnome-terminal gnome-control-center gnome-tweaks gnome-shell-extensions gnome-system-monitor eog celluloid gnome-text-editor nautilus vivaldi vivaldi-ffmpeg-codecs libnotify breeze papirus-icon-theme gnome-backgrounds power-profiles-daemon gnome-calculator ttf-caladea ttf-carlito ttf-dejavu ttf-liberation noto-fonts ttf-opensans otf-overpass ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore pipewire pipewire-pulse pipewire-audio wireplumber bluez bluez-utils mesa lib32-mesa mesa-utils xf86-video-nouveau xf86-video-intel vulkan-intel lib32-vulkan-intel switcheroo-control --noconfirm
+sudo pacman -Sy \ 
+    linux-lts-headers intel-ucode git clang curl wget zsh neovim \
+    xorg xorg-server xdg-user-dirs gnome-shell gdm gnome-terminal gnome-control-center \
+    gnome-tweaks gnome-shell-extensions gnome-system-monitor eog celluloid \
+    gnome-text-editor nautilus vivaldi vivaldi-ffmpeg-codecs libnotify breeze \ 
+    papirus-icon-theme gnome-backgrounds power-profiles-daemon gnome-calculator \
+    ttf-caladea ttf-carlito ttf-dejavu ttf-liberation noto-fonts ttf-opensans \
+    otf-overpass ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family a52dec faac \
+    faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv \
+    wavpack x264 xvidcore pipewire pipewire-pulse pipewire-audio wireplumber bluez \
+    bluez-utils mesa lib32-mesa mesa-utils xf86-video-nouveau xf86-video-intel \
+    vulkan-intel lib32-vulkan-intel switcheroo-control \
+  --noconfirm
 
 xdg-user-dirs-update
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended --keep-zshrc"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) \
+  --unattended --keep-zshrc"
 cp ./assets/zshrc ~/.zshrc
 chsh -s $(which zsh)
 
