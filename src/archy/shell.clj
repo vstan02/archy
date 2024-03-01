e944ef5ac7431a0e2b98e5fc5ebcf187099f2dc2
Stan Vlad <63295109+vstan02@users.noreply.github.com>
2024-03-01 09:12:28 +0200

    Removed shell/cmds

--- src/archy/shell.clj
+++ src/archy/shell.clj
@@ -2,45 +2,24 @@
   (:gen-class)
   (:require [archy.code :as code]))
 
-(def ^:private cmds
-  {:add-modules
-   (code/lines "sudo sed -i \"s/MODULES=(/MODULES=(%1$s /g\" /etc/mkinitcpio.conf"
-               "sudo mkinitcpio -P")
-   :pacman-sync
-   (code/lines "sudo pacman -Sy --noconfirm")
-   :pacman-clean
-   (code/lines "sudo pacman -Rs $(pacman -Qdtq) --noconfirm"
-               "sudo pacman -Sc --noconfirm")
-   :makepkg
-   (code/lines "makepkg -sri")
-   :add-service
-   (code/lines "sudo systemctl enable %1$s.service")
-   :pkg-custom-install
-   (code/lines "cd ~/Downloads"
-               "git clone '%2$s' '%1$s'"
-               "cd '%1$s'"
-               "%3$s"
-               "cd .."
-               "sudo rm -R '%1$s'"
-               "cd ~")
-   :pkg-pacman-install
-   (code/lines "sudo pacman -S %1$s --noconfirm")
-   :pkg-yay-install
-   (code/lines "yay -S %1$s --noconfirm")})
-
 (defn pacman-sync []
-  (:pacman-sync cmds))
+  (code/lines "sudo pacman -Sy --noconfirm"))
 
 (defn pacman-clean []
-  (:pacman-clean cmds))
+  (code/lines "sudo pacman -Rs $(pacman -Qdtq) --noconfirm"
+               "sudo pacman -Sc --noconfirm"))
 
 (defn add-modules [modules]
   (->> modules
        (apply code/words)
-       (format (:add-modules cmds))))
+       (format 
+        (code/lines "sudo sed -i \"s/MODULES=(/MODULES=(%1$s /g\" /etc/mkinitcpio.conf"
+                    "sudo mkinitcpio -P"))))
 
 (defn add-service [service]
-  (format (:add-service cmds) service))
+  (format 
+   (code/lines "sudo systemctl enable %1$s.service") 
+   service))
 
 (defn add-services [services]
   (->> services
@@ -50,16 +29,24 @@
 (defn pacman-install [& names]
   (->> names
        (apply code/words)
-       (format (:pkg-pacman-install cmds))))
+       (format (code/lines "sudo pacman -S %1$s --noconfirm"))))
 
 (defn yay-install [& names]
   (->> names
        (apply code/words)
-       (format (:pkg-yay-install cmds))))
+       (format (code/lines "yay -S %1$s --noconfirm"))))
 
 (defn custom-install [name, repo-url, install-cmds]
-  (format (:pkg-custom-install cmds)
-          name repo-url install-cmds))
+  (format 
+   (code/lines "cd ~/Downloads"
+               "git clone '%2$s' '%1$s'"
+               "cd '%1$s'"
+               "%3$s"
+               "cd .."
+               "sudo rm -R '%1$s'"
+               "cd ~")
+   name repo-url install-cmds))
 
 (defn makepkg-install [name repo-url]
-  (custom-install name repo-url (:makepkg cmds)))
+  (custom-install name repo-url 
+   (code/lines "makepkg -sri")))
