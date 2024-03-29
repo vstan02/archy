ef5a6aaba50eafd8bd4c59ed857ee71f1d9b3ace
Stan Vlad <vstan02@protonmail.com>
2024-03-29 16:58:28 +0200

    Updated index.json

--- examples/script.sh
+++ examples/script.sh
@@ -1,15 +1,15 @@
 sudo pacman -Sy --noconfirm
 
-sudo pacman -S linux-lts-headers intel-ucode git clang curl wget zsh neovim xorg xorg-server gnome-shell gdm gnome-terminal gnome-control-center gnome-tweaks gnome-shell-extensions gnome-system-monitor eog celluloid gnome-backgrounds power-profiles-daemon gnome-calculator gnome-text-editor nautilus xdg-user-dirs pipewire pipewire-pulse pipewire-audio wireplumber bluez bluez-utils a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore ttf-caladea ttf-carlito ttf-dejavu ttf-liberation noto-fonts ttf-opensans otf-overpass ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family mesa lib32-mesa mesa-utils xf86-video-nouveau xf86-video-intel vulkan-intel lib32-vulkan-intel switcheroo-control breeze papirus-icon-theme vivaldi vivaldi-ffmpeg-codecs libnotify --noconfirm
+sudo pacman -S vivaldi vivaldi-ffmpeg-codecs libnotify xorg xorg-server gnome-shell gdm gnome-terminal gnome-control-center gnome-tweaks gnome-shell-extensions gnome-system-monitor eog celluloid gnome-backgrounds power-profiles-daemon gnome-calculator gnome-text-editor nautilus xdg-user-dirs pipewire pipewire-pulse pipewire-audio wireplumber bluez bluez-utils breeze papirus-icon-theme a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore obsidian pinta linux-lts-headers intel-ucode git clang curl wget zsh neovim steam ttf-caladea ttf-carlito ttf-dejavu ttf-liberation noto-fonts ttf-opensans otf-overpass ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-jetbrains-mono-nerd mesa lib32-mesa mesa-utils xf86-video-nouveau xf86-video-intel vulkan-intel lib32-vulkan-intel switcheroo-control --noconfirm
+
+xdg-user-dirs-update
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended --keep-zshrc"
 cp ./assets/zshrc ~/.zshrc
 chsh -s $(which zsh)
 
-mkdir -p ~/.config/nvim
-cp ./assets/vimrc ~/.config/nvim/init.vim
-
-xdg-user-dirs-update
+git clone 'https://github.com/vstan02/nvim-config.git' ~/.config/nvim
+sudo rm -R ~/.config/nvim/.git
 
 cd ~/Downloads
 git clone 'https://aur.archlinux.org/yay.git' 'yay'
